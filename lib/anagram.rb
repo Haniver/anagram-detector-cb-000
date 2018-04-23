0e95503d8c70d5de8421cff92d8d6ca67dda2bf9
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-23 20:34:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,8 @@ class Anagram
   def match(anagrams)
     matches = []
     anagrams.each do |anagram|
-      if anagram.size == @word.size
-        if @word.split('').all? {|letter_in_word| @word.count(letter_in_word) == anagram.count(letter_in_word)}
-          matches << anagram
-        end
+      if anagram.sort == @word.sort
+        matches << anagram
       end
     end
     matches
